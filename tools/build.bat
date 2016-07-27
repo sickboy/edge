f2cac52cae805a917cd78d183364fec9084bed1b
Patrick Roza <roza@sixnetworks.net>
2016-07-27 13:56:25 +0200

    Support Electron 1.3.1

--- tools/build.bat
+++ tools/build.bat
@@ -54,7 +54,7 @@ if not exist "%GYP%" (
     exit /b -1
 )
 
-"%NODEEXE%" "%GYP%" configure build --msvs_version=2015 -%FLAVOR%
+"%NODEEXE%" "%GYP%" configure build --msvs_version=2015 --target=1.3.1 --dist-url=https://atom.io/download/atom-shell -%FLAVOR%
 if %ERRORLEVEL% neq 0 (
     echo Error building edge.node %FLAVOR% for node.js %2 v%3
     exit /b -1
